3bde0c7ecceda1122704eaf57f0929a0c3099c78
oshinosenko <senchoism@gmail.com>
2026-04-21 14:52:37 +0700

    Add initial route definitions, console commands, and testing setup

---
+++ resources/views/test_method.blade.php
@@ -0,0 +1,23 @@
+<form action="/simpan" method="POST">
+    @csrf
+    <button type="submit">Simpan Data (POST)</button>
+</form>
+
+<form action="/update/1" method="POST">
+    @csrf
+    @method('PUT')
+    <button type="submit">Update Data (PUT)</button>
+</form>
+
+<form action="/update2/1" method="POST">
+    @csrf
+    @method('PATCH')
+    <button type="submit">Update Data (PATCH)</button>
+</form>
+
+<form action="/hapus/1" method="POST">
+    @csrf
+    @method('DELETE')
+    <button type="submit">Hapus Data (DELETE)</button>
+</form>
+
---
+++ routes/web.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//route 
+Route::get('/alamat', function() {
+    echo "jalan rajawali 14.palembang";
+    echo "<br>";
+    echo "bejir keren banget gwa";
+});
+
+//route dinamis dengan parameter id
+Route::get('/user/{id}', function() {
+    echo "User id:" . $id;
+});
+
+//route dinamis dengan parameter nama
+Route::get('/user1/{nama}', function() {
+    echo "User name:" . $nama;
+});
+
+//route dinamis dengan opsional parameter nama
+Route::get('/user2/{nama?}', function($name = 'tamu') {
+    echo "User name:" . $name;
+});
+
+//route dinamis dengan parameter nama dan id
+Route::get('/user3/{nama}', function($id, $name) {
+    echo "User ID:" . $id;
+    echo "<br>";
+    echo "User name:" .$name;
+});
+
+################## Bagian paling penting disini ##################
+
+//Router dengan metode POST
+Route::post('/simpan', function(){
+    echo "Data berhasil disimpan";
+});
+ 
+//Router dengan metode PUT
+Route::put('/update/{id}', function($id){
+    echo "Data berhasil diperbarui dengan ID: " . $id;
+});
+ 
+//Router dengan metode PATCH
+Route::patch('/update2/{id}', function($id){
+    echo "Data berhasil diperbarui dengan ID: " . $id;
+});
+ 
+//Router dengan metode DELETE
+Route::delete('/hapus/{id}', function($id){
+    echo "Data berhasil dihapus dengan ID: " . $id;
+});
+ 
+//Route untuk menampilkan halaman test_method
+Route::get('/test-method', function(){
+    return view('test_method');
+});
+ 
